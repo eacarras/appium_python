39fd88b2b88c643b2f944aabe768c493c30728db
eacarras <enriqueaaron7@gmail.com>
2018-06-18 20:50:33 -0500

    Refactor and clean the code

--- androidTest.py
+++ androidTest.py
@@ -1,25 +1,30 @@
 __author__ = 'eacarras'
+__name__ = '__main__'
 import unittest
 from appium import webdriver
+import os
+from time import sleep
 
 
 class AndroidTest(unittest.TestCase):
 
     def setUp(self):
         desired_caps = {}
-        desired_caps["platformName"] = "Android"
-        desired_caps["platformVersion"] = "6.0.1"
-        desired_caps["deviceName"] = "Galaxy"
+        desired_caps["platformName"] = os.environ[""]
+        desired_caps["platformVersion"] = os.environ[""]
+        desired_caps["deviceName"] = os.environ[""]
+
         desired_caps["browserName"] = "Chrome"
 
         self.driver = webdriver.Remote("http://localhost:4723/wb/hub", desired_caps)
 
+    # Here you put all the smoke/regression/bvt test
+    def simpleSmokeTest(self):
+        self.driver.get("http://m.youtube.com")
+        
     # Compile the test
     def tearDown(self):
         self.driver.quit()
-
-    def simpleSmokeTest(self):
-        self.driver.get("http://m.youtube.com")
 
 
 if __name__ == '__main__':
---
+++ variables.py
@@ -0,0 +1,10 @@
+# This code is for generate the environment variables that need the program
+import os
+
+
+os.environ["Name_android"] = "android"
+os.environ["Name_ios"] = "put here the info"
+os.environ["Version_android"] = "6.0.1"
+os.environ["Version_ios"] = "put here the info"
+os.environ["device_android"] = "j1minive3g"
+os.environ["device_ios"] = "put here the info";
